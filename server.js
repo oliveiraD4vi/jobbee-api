dfb2308c58ef01f4848e9aedf9021c8909486bd1
Davi Oliveira <davi.comp.oliveira@gmail.com>
2023-03-17 10:43:31 -0300

    feat(routes): added first route for the base request

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+require("./src/routes/index")(app);
+
+app.listen(process.env.PORT | 3000);
