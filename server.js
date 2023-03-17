000e0cba5fddc8507cb29946f98e78b9c20ea7b8
Davi Oliveira <davi.comp.oliveira@gmail.com>
2023-03-17 11:06:59 -0300

    feat(environment): added envinronment variables config

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+// Imports
 const express = require("express");
 const cors = require("cors");
 const app = express();
 
+// App configuration
 app.use(cors());
 app.use(express.json());
 
+// Required imports
+require("dotenv/config");
 require("./src/routes/index")(app);
 
+// Server initialization
 app.listen(process.env.PORT | 3000);
