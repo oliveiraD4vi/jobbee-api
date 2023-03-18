c5a0f3c78f1e8bcd13d54c82b815af0f2670bc18
Davi Oliveira <davi.comp.oliveira@gmail.com>
2023-03-18 15:50:17 -0300

    feat(controllers): added new functonality of search by a job within a range

--- src/routes/controllers/jobs/controller.js
+++ src/routes/controllers/jobs/controller.js
@@ -1,4 +1,5 @@
 const Job = require("../../../models/jobs/jobs");
+const geoCoder = require("../../../utils/geocoder");
 
 exports.getJobs = async (req, res, next) => {
   const jobs = await Job.find({});
@@ -6,6 +7,7 @@ exports.getJobs = async (req, res, next) => {
   res.status(200).json({
     error: false,
     message: "Success",
+    results: jobs.length,
     data: jobs,
   });
 };
@@ -17,5 +19,30 @@ exports.newJob = async (req, res, next) => {
     error: false,
     message: "Job created",
     data: job,
+  });
+};
+
+exports.getJobsInRadius = async (req, res, next) => {
+  const { zipcode, distance } = req.params;
+
+  const loc = await geoCoder.geocode(zipcode);
+  const latitude = loc[0].latitude;
+  const longitude = loc[0].longitude;
+
+  const radius = distance / 3963;
+
+  const jobs = await Job.find({
+    location: {
+      $geoWithin: {
+        $centerSphere: [[longitude, latitude], radius],
+      },
+    },
+  });
+
+  res.status(200).json({
+    error: false,
+    message: "Success",
+    results: jobs.length,
+    data: jobs,
   });
 };
--- src/routes/jobs/jobs.js
+++ src/routes/jobs/jobs.js
@@ -1,6 +1,11 @@
-const { getJobs, newJob } = require("../controllers/jobs/controller");
+const {
+  getJobs,
+  newJob,
+  getJobsInRadius,
+} = require("../controllers/jobs/controller");
 
 module.exports = (router) => {
   router.route("/jobs").get(getJobs);
   router.route("/jobs").post(newJob);
+  router.route("/jobs/:zipcode/:distance").get(getJobsInRadius);
 };
