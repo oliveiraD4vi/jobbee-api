eb41bcd1603e6109e4a9a7677830ba02f8c84d51
Davi Oliveira <davi.comp.oliveira@gmail.com>
2023-03-26 09:30:44 -0300

    feat(middlewares): added role verification on Auth middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -21,3 +21,18 @@ exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
 
   return next();
 });
+
+exports.authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorHandler(
+          `Role ${req.user.role} is not allowed to access this resource`,
+          403
+        )
+      );
+    }
+
+    return next();
+  };
+};
--- src/routes/jobs/jobs.js
+++ src/routes/jobs/jobs.js
@@ -1,4 +1,7 @@
-const { isAuthenticatedUser } = require("../../middlewares/auth");
+const {
+  isAuthenticatedUser,
+  authorizeRoles,
+} = require("../../middlewares/auth");
 const {
   getAllJobs,
   createNewJob,
@@ -12,13 +15,33 @@ const {
 module.exports = (router) => {
   // GET
   router.route("/jobs").get(getAllJobs);
-  router.route("/stats/:topic").get(isAuthenticatedUser, getJobStats);
+  router
+    .route("/stats/:topic")
+    .get(
+      isAuthenticatedUser,
+      authorizeRoles("employeer", "admin"),
+      getJobStats
+    );
   router.route("/jobs/:id/:slug").get(getJobByIdSlug);
   router.route("/jobs/radius/:zipcode/:distance").get(getJobsInRadius);
   // POST
-  router.route("/jobs").post(isAuthenticatedUser, createNewJob);
+  router
+    .route("/jobs")
+    .post(
+      isAuthenticatedUser,
+      authorizeRoles("employeer", "admin"),
+      createNewJob
+    );
   // PUT
-  router.route("/jobs/:id").put(isAuthenticatedUser, updateJob);
+  router
+    .route("/jobs/:id")
+    .put(isAuthenticatedUser, authorizeRoles("employeer", "admin"), updateJob);
   // DELETE
-  router.route("/jobs/:id").delete(isAuthenticatedUser, deleteJob);
+  router
+    .route("/jobs/:id")
+    .delete(
+      isAuthenticatedUser,
+      authorizeRoles("employeer", "admin"),
+      deleteJob
+    );
 };
